600790dfa8746371ea0a39da3e9781433bc8f3e2
Rahul Sahani <yourahul1917@gmail.com>
2024-08-20 21:14:38 +0530

    Added assets and fonts to Uber Clone App

---
+++ UberApp/app/(root)/(tabs)/profile.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Profile = () => {
+    return (
+        <SafeAreaView className="flex-1 justify-center items-center">
+            <Text className="text-6xl">Profile</Text>
+        </SafeAreaView>
+    );
+}
+
+export default Profile;
