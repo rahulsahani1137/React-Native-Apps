af6cfb04ba0a00ae29162e340dbdd07df389b8e9
Rahul Sahani <yourahul1917@gmail.com>
2025-04-24 21:40:55 +0530

    intiated the uber app

--- UberApp/app/_layout.tsx
+++ UberApp/app/_layout.tsx
@@ -1,15 +1,19 @@
-import { useFonts } from 'expo-font';
+import '../global.css';
+
 import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
-import 'react-native-reanimated';
+import * as SplashScreen from "expo-splash-screen";
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
+export default function Layout() {
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/PlusJakartaSans-Regular.ttf'),
+    "Jakarta-Bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+    "Jakarta-ExtraBold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+    "Jakarta-ExtraLight": require("../assets/fonts/PlusJakartaSans-ExtraLight.ttf"),
+    "Jakarta-Light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
+    "Jakarta-Medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+    Jakarta: require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "Jakarta-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
   });
 
   useEffect(() => {
@@ -23,12 +27,8 @@ export default function RootLayout() {
   }
 
   return (
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="(root)" options={{ headerShown: false }} />
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    
+    <Stack>
+      <Stack.Screen name='index' options={{ headerShown: false }} />
+    </Stack>
   );
 }
--- UberApp/app/index.tsx
+++ UberApp/app/index.tsx
@@ -1,12 +1,13 @@
-import { Text } from "react-native";
+import { View, Text } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
-    return (
-        <SafeAreaView className="flex-1 justify-center items-center">
-            <Text className="text-6xl">Home</Text>
-        </SafeAreaView>
-    );
+  
+  return (
+    <SafeAreaView>
+      <Text>Hello</Text>
+    </SafeAreaView>
+  )
 }
 
 export default Home;
