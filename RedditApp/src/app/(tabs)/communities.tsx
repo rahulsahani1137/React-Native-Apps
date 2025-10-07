f1417aeddadc5d47764e491e417b0a88ced8f14b
Rahul Sahani <66432121+rahulsahani1137@users.noreply.github.com>
2025-10-07 16:55:15 +0530

    feat:[RedditApp]: Home screen finish wiith expo router

---
+++ RedditApp/src/app/(tabs)/communities.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CommunitiesScreen = () => {
+  return (
+    <View>
+      <Text>CommunitiesScreen</Text>
+    </View>
+  )
+}
+
+export default CommunitiesScreen
+
+const styles = StyleSheet.create({})
---
+++ RedditApp/src/app/(tabs)/index.tsx
@@ -0,0 +1,19 @@
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import PostListItem from '../../components/PostListItem';
+import posts from '../../../assets/data/posts.json'
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <FlatList
+                data={posts}
+                renderItem={({ item }) => <PostListItem post={item} />}
+            />
+        </View>
+    )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
